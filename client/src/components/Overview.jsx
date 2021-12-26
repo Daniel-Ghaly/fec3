609b0abb2f73e94d91ec7879590ab61efdee52d3
Daniel-Ghaly <ghalydan@gmail.com>
2021-12-25 17:26:26 -0800

    added Price + using API

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import StarRating from './OverviewComponents/StarRating.js';
 import Reviews from './OverviewComponents/Reviews.js';
+import axios from 'axios'
+
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      numberOfReviews: null,
+      rating: null,
+      productName: null,
+      productCategory: null,
+      productPrice: null
+    }
+  }
+
+  componentDidMount() {
+
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344', {
+      headers: {
+        authorization: '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'
+      }
+    })
+    .then(res => {
+      this.setState({numberOfReviews: res.data.results.length})
+
+      var sum = 0;
+      res.data.results.forEach(item => {
+        sum += item.rating
+      })
+      var rating = sum/(res.data.results.length)
+      this.setState({rating: rating})
+    })
+
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344', {
+      headers: {
+        authorization: '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'
+      }
+    })
+    .then(res => {
+      this.setState({productName: res.data.name})
+      this.setState({productCategory: res.data.category})
+      this.setState({productCategory: res.data.category})
+      this.setState({productPrice: res.data.default_price})
+
+    })
   }
 
   render () {
+
+
     return (
       <>
-        <StarRating rating ={3.25}/>
-        <Reviews numberOfReviews = {9} />
-        <div id = 'category-overview'>Category</div>
-        <div id = 'product-name-overview'>Product Name</div>
+        <StarRating rating ={this.state.rating}/>
+        <Reviews numberOfReviews = {this.state.numberOfReviews} />
+        <div id = 'category-overview'>{this.state.productCategory}</div>
+        <div id = 'product-name-overview'>{this.state.productName}</div>
+        <div id = 'price-overview'>${this.state.productPrice}</div>
         {/* <Price/> */}
       </>
     )
