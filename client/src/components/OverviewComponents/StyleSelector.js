688fa00408f59adc40620beaf444d0d608f64f85
Daniel-Ghaly <ghalydan@gmail.com>
2021-12-28 09:33:18 -0800

    1

--- client/src/components/OverviewComponents/StyleSelector.js
+++ client/src/components/OverviewComponents/StyleSelector.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import gray from '/Users/danielghaly/Desktop/Hack Reactor/fec3/client/src/components/OverviewComponents/grey-box.png'
 import axios from 'axios'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import checkmark from '/Users/danielghaly/Desktop/Hack Reactor/fec3/client/src/components/OverviewComponents/stars/unnamed (3).jpeg'
 
 class StyleSelector extends React.Component {
   constructor(props) {
@@ -24,15 +27,22 @@ class StyleSelector extends React.Component {
 
     for (var i = 0; i < this.props.thumbnails.length; i++) {
 
+      var element;
       var index = i
-      output.push(<img className = 'added'  onClick = {() => {this.props.handleStyleClick(index)}} src = {this.props.thumbnails[i]} />)
+
+      // if (this.state.selectedStyleIndex === index) {
+      //   element = [<div id = 'image-container'> <img className = 'thumbnail-image' onClick = {() => {this.props.handleStyleClick(index)}} src = {this.props.thumbnails[i]} /> <img className = 'checkmark' src ={checkmark} /> </div>]
+      // }
+      element = [<div id = 'image-container'> <img className = 'thumbnail-image' onClick = {() => {this.props.handleStyleClick(index)}} src = {this.props.thumbnails[i]} /> <img className = 'checkmark' src ={checkmark} /> </div>]
+
+      output.push(element[0])
     }
 
     return (
       <>
       <div id = 'style-container'>
         <div id = 'selected-style'>Style: (Selected Style)</div>
-        <div id = 'img-container'>
+        <div id = 'styles-container'>
         {output}
         </div>
       </div>
