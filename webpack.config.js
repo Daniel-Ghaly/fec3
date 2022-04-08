8a65f423dce90d5daad7bf486649e1f2834ac894
Daniel-Ghaly <ghalydan@gmail.com>
2022-04-08 15:52:01 -0700

    config

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
 
 
   plugins: [
-    new DefinePlugin({
-      'process.env': JSON.stringify(process.env),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   entry: `${SRC_DIR}/index.js`,
