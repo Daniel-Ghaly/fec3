2f023442d03dd2669c8572bf2e604361e135820c
Daniel-Ghaly <ghalydan@gmail.com>
2022-04-08 15:40:00 -0700

    config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var SRC_DIR = path.join(__dirname, 'client/src');
 var DIST_DIR = path.join(__dirname, 'client/dist');
 
+const webpack = require('webpack');
 
 module.exports = {
 
